aa910d02bb77af23bbad167b1f27c7eace8ef26f
daydreamer-json <100826019+daydreamer-json@users.noreply.github.com>
2022-11-24 14:12:05 +0900

    Fix libvulkan-dev issue

--- ncnn-vulkan.sh
+++ ncnn-vulkan.sh
@@ -2,7 +2,7 @@
 mkdir /ncnn_bin
 cd /ncnn_bin
 sudo apt update
-sudo apt install curl unzip
+sudo apt install curl unzip libvulkan-dev
 curl -SsLo waifu2x.zip https://github.com/nihui/waifu2x-ncnn-vulkan/releases/download/20220728/waifu2x-ncnn-vulkan-20220728-ubuntu.zip
 curl -SsLo realesrgan.zip https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan/releases/download/v0.2.0/realesrgan-ncnn-vulkan-v0.2.0-ubuntu.zip
 curl -SsLo realcugan.zip https://github.com/nihui/realcugan-ncnn-vulkan/releases/download/20220728/realcugan-ncnn-vulkan-20220728-ubuntu.zip
