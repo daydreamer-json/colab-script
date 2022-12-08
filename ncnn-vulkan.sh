3180e0ca263547ddc93bd5f5ae612498e5a60768
daydreamer-json <100826019+daydreamer-json@users.noreply.github.com>
2022-12-08 12:23:04 +0900

    Update ncnn-vulkan.sh

--- ncnn-vulkan.sh
+++ ncnn-vulkan.sh
@@ -3,16 +3,16 @@ mkdir /ncnn_bin
 cd /ncnn_bin
 sudo apt update
 sudo wget -q -nc -O /usr/local/bin/curl https://github.com/moparisthebest/static-curl/releases/latest/download/curl-amd64
-sudo apt install unzip libvulkan-dev
-curl -SsLo waifu2x.zip https://github.com/nihui/waifu2x-ncnn-vulkan/releases/download/20220728/waifu2x-ncnn-vulkan-20220728-ubuntu.zip
-curl -SsLo realesrgan.zip https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan/releases/download/v0.2.0/realesrgan-ncnn-vulkan-v0.2.0-ubuntu.zip
-curl -SsLo realcugan.zip https://github.com/nihui/realcugan-ncnn-vulkan/releases/download/20220728/realcugan-ncnn-vulkan-20220728-ubuntu.zip
-curl -SsLo srmd.zip https://github.com/nihui/srmd-ncnn-vulkan/releases/download/20220728/srmd-ncnn-vulkan-20220728-ubuntu.zip
-curl -SsLo realsr.zip https://github.com/nihui/realsr-ncnn-vulkan/releases/download/20220728/realsr-ncnn-vulkan-20220728-ubuntu.zip
-curl -SsLo dain.zip https://github.com/nihui/dain-ncnn-vulkan/releases/download/20220728/dain-ncnn-vulkan-20220728-ubuntu.zip
-curl -SsLo cain.zip https://github.com/nihui/cain-ncnn-vulkan/releases/download/20220728/cain-ncnn-vulkan-20220728-ubuntu.zip
-curl -SsLo rife.zip https://github.com/nihui/rife-ncnn-vulkan/releases/download/20221029/rife-ncnn-vulkan-20221029-ubuntu.zip
-curl -SsLo ifrnet.zip https://github.com/nihui/ifrnet-ncnn-vulkan/releases/download/20220720/ifrnet-ncnn-vulkan-20220720-ubuntu.zip
+sudo apt install unzip libvulkan-dev aria2
+aria2c -q -x16 -s16 -o waifu2x.zip https://github.com/nihui/waifu2x-ncnn-vulkan/releases/download/20220728/waifu2x-ncnn-vulkan-20220728-ubuntu.zip
+aria2c -q -x16 -s16 -o realesrgan.zip https://github.com/xinntao/Real-ESRGAN-ncnn-vulkan/releases/download/v0.2.0/realesrgan-ncnn-vulkan-v0.2.0-ubuntu.zip
+aria2c -q -x16 -s16 -o realcugan.zip https://github.com/nihui/realcugan-ncnn-vulkan/releases/download/20220728/realcugan-ncnn-vulkan-20220728-ubuntu.zip
+aria2c -q -x16 -s16 -o srmd.zip https://github.com/nihui/srmd-ncnn-vulkan/releases/download/20220728/srmd-ncnn-vulkan-20220728-ubuntu.zip
+aria2c -q -x16 -s16 -o realsr.zip https://github.com/nihui/realsr-ncnn-vulkan/releases/download/20220728/realsr-ncnn-vulkan-20220728-ubuntu.zip
+aria2c -q -x16 -s16 -o dain.zip https://github.com/nihui/dain-ncnn-vulkan/releases/download/20220728/dain-ncnn-vulkan-20220728-ubuntu.zip
+aria2c -q -x16 -s16 -o cain.zip https://github.com/nihui/cain-ncnn-vulkan/releases/download/20220728/cain-ncnn-vulkan-20220728-ubuntu.zip
+aria2c -q -x16 -s16 -o rife.zip https://github.com/nihui/rife-ncnn-vulkan/releases/download/20221029/rife-ncnn-vulkan-20221029-ubuntu.zip
+aria2c -q -x16 -s16 -o ifrnet.zip https://github.com/nihui/ifrnet-ncnn-vulkan/releases/download/20220720/ifrnet-ncnn-vulkan-20220720-ubuntu.zip
 unzip waifu2x.zip; unzip realesrgan.zip; unzip realcugan.zip; unzip srmd.zip; unzip realsr.zip; unzip dain.zip; unzip cain.zip; unzip rife.zip; unzip ifrnet.zip
 rm *.zip
 rename -v 's/-ncnn-vulkan.*/-ncnn-vulkan/' *-ncnn-vulkan*
