c87e9b007e168a2c9db4cbc0619a9138ef7fdd5c
daydreamer-json <100826019+daydreamer-json@users.noreply.github.com>
2022-12-23 02:48:20 +0900

    Create mediainfo.sh

---
+++ mediainfo.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo Downloading package ...
+curl -SsLo libzen.deb https://mediaarea.net/download/binary/libzen0/0.4.39/libzen0v5_0.4.39-1_amd64.xUbuntu_18.04.deb
+curl -SsLo libmediainfo.deb https://mediaarea.net/download/binary/libmediainfo0/22.09/libmediainfo0v5_22.09-1_amd64.xUbuntu_18.04.deb
+curl -SsLo mediainfo_cli.deb https://mediaarea.net/download/binary/mediainfo/22.09/mediainfo_22.09-1_amd64.xUbuntu_18.04.deb
+echo Updating package cache ...
+sudo apt update > /dev/null 2>&1
+echo Installing package ...
+sudo apt install ./libzen.deb ./libmediainfo.deb ./mediainfo_cli.deb -y > /dev/null 2>&1
+echo Complete.
